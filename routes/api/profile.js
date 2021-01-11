0efb5c37ecb487dc6111993b6dc70c4ce93187f8
Muhammad Hamza <megahertz1999@gmail.com>
2021-01-11 07:36:46 +0500

    added education add/delete routes

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -236,4 +236,81 @@ router.delete('/experience/:id', auth, async (req, res) => {
   }
 });
 
+//Education
+
+/*
+@route  PUT api/profile/education
+@desc   Add education of a user in profile
+@access Private
+*/
+router.put(
+  '/education',
+  [
+    auth,
+    [
+      check('school', 'School is required').not().isEmpty(),
+      check('degree', 'Degree is required').not().isEmpty(),
+      check('fieldofstudy', 'Field of study is required').not().isEmpty(),
+      check('from', 'From date is required').not().isEmpty(),
+    ],
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    const {
+      school,
+      degree,
+      fieldofstudy,
+      from,
+      to,
+      description,
+      current,
+    } = req.body;
+
+    const newEdu = {
+      school,
+      degree,
+      fieldofstudy,
+      from,
+      to,
+      current,
+      description,
+    };
+
+    try {
+      const profile = await Profile.findOne({ user: req.user.id });
+      profile.education.unshift(newEdu);
+      await profile.save();
+      res.json(profile);
+    } catch (error) {
+      console.error(error.message);
+      res.status(500).json({ msg: 'Server Error' });
+    }
+  }
+);
+
+/*
+@route  DELETE api/profile/education/:id
+@desc   Delete education of a user in profile
+@access Private
+*/
+router.delete('/education/:id', auth, async (req, res) => {
+  try {
+    const profile = await Profile.findOne({ user: req.user.id });
+
+    const index = profile.education
+      .map((exp) => exp._id)
+      .indexOf(req.params.id);
+    profile.education.splice(index, 1);
+    await profile.save();
+    return res.json(profile);
+  } catch (error) {
+    console.error(error.message);
+    return res.status(500).json({ msg: 'Server Error' });
+  }
+});
+
 module.exports = router;
