72fb987a996768a4ee3ba21d9492ed06091b6cf1
Muhammad Hamza <megahertz1999@gmail.com>
2021-01-23 07:42:58 +0500

    added layouts, login,register links

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,25 @@
+import React, { Fragment } from 'react';
 import './App.css';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import Landing from './components/layouts/Landing';
+import Navbar from './components/layouts/Navbar';
+import Login from './components/auth/Login';
+import Register from './components/auth/Register';
 
 const App = () => {
   return (
-    <>
-      <h1>App</h1>
-    </>
+    <BrowserRouter>
+      <Fragment>
+        <Navbar />
+        <Route exact path='/' component={Landing} />
+        <section className='container'>
+          <Switch>
+            <Route exact path='/login' component={Login} />
+            <Route exact path='/register' component={Register} />
+          </Switch>
+        </section>
+      </Fragment>
+    </BrowserRouter>
   );
 };
 
